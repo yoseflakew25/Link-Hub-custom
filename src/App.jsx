a20132d5222f636c8589af844b2afd1b4341b099
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-10-15 13:48:00 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
         {/* ------------- IMAGE ----- START */}
         <div className=" flex items-center justify-center gap-2 flex-col">
           <img
-            className=" mix-blend-difference w-[100px] rounded-full bg-black "
+            className="w-[100px] rounded-full bg-black "
             src="/name logo.jpg"
             alt=""
           />
